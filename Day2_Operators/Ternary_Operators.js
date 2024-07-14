521a2336f4ece7c131f2b16d827b572fc64f2671
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-15 00:10:01 +0530

    add Teranry-operators task

---
+++ Day2_Operators/Ternary_Operators.js
@@ -0,0 +1,4 @@
+// Task 14 : write a program that uses ternary operators to check if a number is positive or negative and log the result to the console.
+let num=-10;
+let checkPostiveNegativeNum=num>=0 ? 'num is postive ':'num is negative';
+console.log(checkPostiveNegativeNum)
