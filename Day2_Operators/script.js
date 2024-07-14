4ca6a083868b5ba6f18d6bf632a812c00f3f8848
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-14 23:51:15 +0530

    add arithmetic file

---
+++ Day2_Operators/script.js
@@ -0,0 +1,43 @@
+// Activity 1:Arithmetic Operations
+// Task 1: write a program to add numbers and log the result to the console
+
+
+let num1=30;
+let num2=28;
+let sums=num1 + num2
+console.log(sums);
+
+
+// Task2 : Write a program to subtract two numbers and log the result to the console
+let substract =num1 - num2 ;
+console.log(substract);
+
+
+// Task 3 : Write a program to mulitply two numbers and log the result to the console
+let multiplication=num1*num2;
+console.log(multiplication);
+
+// Task 4 : Write a program to divide two numbers and log the result to the console 
+
+let divideResult=num1/num2;
+console.log(divideResult);
+
+// Task 5 : Write a program to find remainder when one numbers is divided by another and log the result to the console
+let remainder =num1%num2;
+console.log(remainder)
+
+
+// Activity 2: Assignment Operators
+// Task 6: Use the += operator to add a number to a variable and log the result to the console
+let a=2;
+let value=3;
+value +=a;//value=value+a;
+console.log(value)
+
+let b="hello"
+console.log(b+=" world");
+
+// Task 7 : Use the -= operator to subtract a number from a variable and log the result to the console.
+let sub=10;
+sub -=a;;//sub=sub-a
+console.log(sub);
