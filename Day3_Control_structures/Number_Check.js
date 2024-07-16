9d104e68328dc9d70ee0350b01cafedde3bb491f
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-16 23:31:20 +0530

    complete day3 tasks

---
+++ Day3_Control_structures/Number_Check.js
@@ -0,0 +1,8 @@
+// Activity 1: if-Else Statements
+// Task 1 : Write a program to check if a number is positive,negative or zero , and log the result to the console.
+let num=20;
+if(num>=0){
+    console.log(  'number is positive');
+}else{
+    console.log(  'number is negaitive');
+}
---
+++ Day3_Control_structures/Voting_Elgibility.js
@@ -0,0 +1,7 @@
+// Task 2 : Write a program to check if a person is eligible to vote (age>=18) and log the result to the console.
+let age =24;
+if(age>=18){
+    console.log("You are Elgiible to vote")
+}else{
+    console.log("you are under age focus to your studies")
+}
