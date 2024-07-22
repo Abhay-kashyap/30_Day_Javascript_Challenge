e6b11bd51e855061e2d49dba0b7bf6a18e4e6d62
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-22 21:54:55 +0530

    add SwitchCaseFile

---
+++ Day3_Control_structures/Day_Of_the_week.js
@@ -0,0 +1,26 @@
+let day = 7;
+switch (day) {
+  case 1:
+    console.log("today is Monday");
+    break;
+  case 2:
+    console.log(`today is tuesday`);
+    break;
+  case 3:
+    console.log(`today is Wednesday`);
+    break;
+  case 4:
+    console.log(`today is thursday`);
+    break;
+  case 5:
+    console.log(`today is Friday`);
+    break;
+  case 6:
+    console.log("today is Saturday");
+    break;
+  case 7:
+    console.log("today is Sunday");
+    break;
+  default:
+    console.log("enter wrong value");
+}
--- Day3_Control_structures/Voting_Elgibility.js
+++ Day3_Control_structures/Voting_Elgibility.js
@@ -5,3 +5,43 @@ if(age>=18){
 }else{
     console.log("you are under age focus to your studies")
 }
+
+// Task 3 : Write a program to find the largest of three numbers using nested if-else statements.
+let num1=20;
+let num2=2;
+let num3=2;
+
+
+// if(num1>num2 && num2>num3){
+//     console.log('num1 is largest number')
+// }else if(num2>num1 &&num2>num3){
+//     console.log(`the largest number is ${num2}`)
+// }else{
+//     console.log( `the largest number is ${num3}`)
+// }
+// if(num1>num2 && num2 > num3){
+//         console.log(`num1 is the largest number ${num1}`)
+    
+// }else{
+//     if(num2>num1 &&num2 >num3){
+
+//         console.log(`num2 is greater than ${num2}`)
+//     }
+//     else{
+//         console.log('num3 is greater than')
+//     }
+// }
+if(num1>num2){
+    if(num1>num3){
+        console.log(`num1 is the largest number ${num1}`);
+    }
+    else{
+        console.log(`num3 is the largest number ${num3}`);
+    }
+}else{
+    if(num2>num3){
+        console.log(`num2 is the largest number ${num2}`);
+    }else{
+        console.log(`num3 is the largest number ${num3}`)
+    }
+}
