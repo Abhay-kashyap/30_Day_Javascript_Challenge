0e6d21462431515931642a8a83a00d3bcc4cc0d0
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-25 11:10:53 +0530

    Add ArrayIteration

---
+++ Day6_Array/ArrayIteration.js
@@ -0,0 +1,9 @@
+// Task 10: Use a for loop to iterate over the array and log each element to the console
+let arr=[1,2,3,4,8,9,7,5,3];
+for(let i=0;i<=arr.length-1;i++){
+    // console.log(arr[i])
+}
+// Task 11 : Use a forEach method to iterate over the array and log each element to the console
+arr.forEach((curEle)=>{
+    console.log(curEle)
+})
