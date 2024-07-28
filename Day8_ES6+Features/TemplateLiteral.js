b74c3723b947c3c39aa0f05f382b1bcec2c6dc1f
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-28 12:45:50 +0530

    add templatefile

---
+++ Day8_ES6+Features/TemplateLiteral.js
@@ -0,0 +1,10 @@
+// Task 1: Use template literals to create a string that includes variables for a person's name and age , and log the string
+const personName="Rahul";
+let age =19;
+console.log(`Using template literals to log the person name : ${personName} and age is ${age}`);
+
+// Task 2: Create a multi-line string using template literals and log it to the console.
+const str=`This is a multiline string.
+It can span multiple lines.
+Here is another line.`;
+console.log(str)
