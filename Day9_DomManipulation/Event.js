f2b36c8aa3c50d7cb73c446d59cf334b0b807617
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-07-29 18:32:09 +0530

    day 9 task complete

---
+++ Day9_DomManipulation/Event.js
@@ -0,0 +1,14 @@
+// Task 9: Add a click event listener to a button that changes the text content of paragraph
+let p=document.getElementById('para');
+console.log(p)
+let btn=document.querySelector('button')
+btn.addEventListener('click',()=>{
+    p.innerText='that paragraph content change by btn click'
+})
+// Task 10 : Add a mouseover event listener to an element that changes its border color
+p.addEventListener('mouseover',()=>{
+    p.style.border='2px solid green'
+})
+p.addEventListener('mouseout',()=>{
+    p.style.border=''
+})
